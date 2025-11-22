517e33eb54590f99fe0d612520182a354941d840
max <maxthomarino@gmail.com>
2025-11-22 23:53:40 +0000

    Created main, will test now.

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,27 @@
 int main() {
 
 	if (!glfwInit()) {
-		std::cerr << "ERROR: glfwInit() failed.\n"
+		std::cerr << "ERROR: glfwInit() failed.\n";
 		return -1;
 	} else {
-		std::cout << "GLFW Initialized.\n"
+		std::cout << "GLFW Initialized.\n";
 	}
-	
+
+	GLFWwindow* window = glfwCreateWindow(800,600,"TITLE",nullptr,nullptr);
+	if (window) {
+		std::cout << "Window created.\n";
+	} else {
+		std::cerr << "Failed to create window.\n";
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();	
+	}
+
+	glfwTerminate();
+	return 0;
 }
