689ee7c3fded5f394496f906d76e60868aca8293
max <maxthomarino@gmail.com>
2025-11-23 18:57:20 +0000

    ImGUI added and working. Need to change where I put it though

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,42 @@
 
 #include "../include/window.hpp"
 
+#include "../external/imgui/imgui.h"
+#include "../external/imgui/backends/imgui_impl_glfw.h"
+#include "../external/imgui/backends/imgui_impl_opengl3.h"
+
+
 int main() {
 
 
 	Window window("Graphics");
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window.getHandle(), true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
 
 	while (window.isOpen()) {
 		window.pollEvents();
 		
 		glClearColor(0.5f,0.5f,0.5f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// minimal test window
+		ImGui::Begin("Hello, ImGui");
+		ImGui::Text("If you see this, ImGui is working.");
+		ImGui::End();
+
+		// render ImGui
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		
 		window.swapBuffers();
 	}
