2e42db8d21aaed606df900840186be10fe3d4d2b
max <dudujuju828@gmail.com>
2025-11-27 21:13:20 +0000

    Added grass blades, and can change amount of grass blades w slider

--- src/engine.cpp
+++ src/engine.cpp
@@ -65,6 +65,21 @@ void Engine::processMouse() {
 void Engine::run() {
     GLFWwindow* glfwWin = window.getHandle();
 
+	/* setting up imgui start */
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io; 
+
+    ImGui::StyleColorsDark();
+	io.FontGlobalScale = 6.0f;            
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.ScaleAllSizes(3.0f);
+    ImGui_ImplGlfw_InitForOpenGL(glfwWin, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+	/* setting up imgui end */
+
     Shader shader("src/vertex_s.glsl", "src/fragment_s.glsl");
     Shader model("src/model_vs.glsl", "src/model_fs.glsl");
     Shader grassShader("src/grass_vs.glsl", "src/grass_fs.glsl");
@@ -114,7 +129,8 @@ void Engine::run() {
 	};
 
 
-    grass.init(15000, xRange, zRange, sampleHeight);
+	int grassBladeCount = 100000;
+    grass.init(grassBladeCount, xRange, zRange, sampleHeight);
 
     glEnable(GL_DEPTH_TEST);
     lastFrameTime = static_cast<float>(glfwGetTime());
@@ -154,6 +170,13 @@ void Engine::run() {
 	}
 	stbi_image_free(data);
 
+	glfwSwapInterval(0);
+
+	float currentFPS = 0.0f;
+	float averageFPS = 0.0f;
+	float totalTime = 0.0f;
+	int frameCount = 0;
+	static float grassDensity = 0.5f;  // Default value
 
     while (!glfwWindowShouldClose(glfwWin)) {
         float currentTime = static_cast<float>(glfwGetTime());
@@ -163,6 +186,32 @@ void Engine::run() {
         window.pollEvents();
         processInput(deltaTime);
         processMouse();
+
+		/* imguiframe stuff */
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+		
+		currentFPS = 1.0f/deltaTime;
+		totalTime += deltaTime;
+		frameCount ++;
+		
+		if (totalTime >= 1.0f) {
+			averageFPS = frameCount / totalTime;
+			totalTime = 0.0f;
+			frameCount = 0;
+		}	
+
+		ImVec2 newWindowSize = ImVec2(1500, 600);  // Set the width to 500 and height to 300
+		ImGui::SetNextWindowSize(newWindowSize, ImGuiCond_Always);
+
+        ImGui::Begin("Stats");
+        ImGui::Text("Current FPS: %.1f", currentFPS);
+        ImGui::Text("Average FPS: %.1f", averageFPS);
+        ImGui::Text("Frame time: %.3f ms", deltaTime * 1000.0f);
+        ImGui::Text("Grass blades: %u", grassBladeCount);
+		/* imguiframe stuff end */
+
 
         window.setColor(0.361f, 0.6f, 1.0f);
 
@@ -186,9 +235,19 @@ void Engine::run() {
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         //glDisable(GL_CULL_FACE);
+		if (ImGui::SliderFloat("Grass Density", &grassDensity, 0.0f, 1.0f)) {
+			// Adjust grass density based on slider value
+			int newBladeCount = static_cast<int>(grassDensity * 1000000);  // Adjust scaling factor as needed
+			grassBladeCount = newBladeCount;
+			grass.init(grassBladeCount, xRange, zRange, sampleHeight);  // Re-initialize grass with new blade count
+		}		
         grass.draw(grassShader, view, projection, currentTime, grassBladeTex);
         //glEnable(GL_CULL_FACE);
         glDisable(GL_BLEND);
+
+        ImGui::End();
+		/* imguiframe stuff end */
+
 
         model.useProgram();
         model.setMat4("view", view);
@@ -197,8 +256,15 @@ void Engine::run() {
         model.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
         model.setVec3("objectColor", glm::vec3(0.43f, 0.21f, 0.08f));
 		treeObject.draw(model);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         window.swapBuffers();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 }
 
 void Engine::processInput(float deltaTime) {
